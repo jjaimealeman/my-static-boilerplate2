293e1a5ea80300e0937ac66fa267ca9cd1ce04de
Jaime Aleman <jjaimealeman@gmail.com>
2022-04-11 23:30:02 -0600

    vite.config.js // update navigation.

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ module.exports = defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, 'index.html'),
-				test: resolve(__dirname, 'test.html'),
-				more: resolve(__dirname, 'more.html')
+				test: resolve(__dirname, '/test/index.html'),
+				more: resolve(__dirname, '/more/index.html')
 			}
 		}
 	}
