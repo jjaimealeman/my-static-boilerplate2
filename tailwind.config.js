0277b8c5095c03050219bf62ea623ccb5c10c510
Jaime Aleman <jjaimealeman@gmail.com>
2023-01-26 22:30:59 -0700

    Wevcome back.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,38 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
-  content: [
-      "./index.html",
-      "./test.html",
-      "./**/*.html"
-  ],
-  theme: {
-    extend: {
-        fontFamily: {
-            'raleway': ['Raleway', 'sans-serif'],
-            'montserrat': ['Montserrat', 'sans-serif']
-        }
-    }
-},
-plugins: [],
+	content: [
+		"./index.html",
+		"./**/*.html"
+	],
+	theme: {
+		debugScreens: {
+			position: ['top', 'right'],
+		},
+
+		// REMOVE EXTEND
+		// TO DISABLE ASSET LOADING OF UNNCECESSARY LOADING
+		// https://blog.logrocket.com/how-to-use-custom-fonts-tailwind-css/
+		//
+		// extend: {
+		//     fontFamily: {
+		//         'raleway': ['Raleway', 'sans-serif'],
+		//         'montserrat': ['Montserrat', 'sans-serif']
+		//     }
+		// }
+
+		fontFamily: {
+			'raleway': ['Raleway', 'sans-serif'],
+			'montserrat': ['Montserrat', 'sans-serif']
+		}
+
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/aspect-ratio'),
+		require('tailwindcss-fluid-type'),
+		require('tailwindcss-debug-screens')
+	],
 }
